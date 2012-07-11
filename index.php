a9f693fc9cdbd12db31d955d199f9cbf993f14de
teff <jonas@example.com>
2012-07-11 22:23:50 +0200

    adding

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="keywords" content="Anlegg, Maskin, Øytunanlegg, Oytunanlegg, Entrepenør" />
+	<meta name="description" content="Anleggsfirma Øytunanlegg" />
+	<title>Øytunanlegg</title>
+	<link rel="stylesheet" type="text/css" href="css/structure.css" />
+</head>
+<body>
+	<div id="container" class="container-wrapper">
+	
+
+
+	</div>
+
+
+</body>
+</html>
